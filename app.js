2d6107dbc66ad09eb0845473d7aea6406d4b7c84
CN77 <cn77@CN77s-MacBook-Pro.local>
2019-08-30 17:20:26 +0100

    myFirst Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// Play drum by keyboard press
+
+// Play music by mouse click
+const boom = document.getElementById("boom")
+const clap = document.getElementById("clap")
+const hihat = document.getElementById("hihat")
+const kick = document.getElementById("kick")
+const openhat = document.getElementById("openhat")
+const ride = document.getElementById("ride")
+const snare = document.getElementById("snare")
+const tink = document.getElementById("tink")
+const tom = document.getElementById("tom")
+
+document.addEventListener("click",()=> {
+    
+    
+})
